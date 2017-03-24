42951696fd107f60c161fa5ff0cb16e4b083f9d8
Jerome Duncan <jerome@bigbitecreative.com>
2017-03-24 14:37:21 +0000

    modify rollup build process

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,19 @@
 import babel from 'rollup-plugin-babel';
 import uglify from 'rollup-plugin-uglify';
 
-export default {
+let buildObj = {
   entry: 'src/macy.js',
   format: 'umd',
   moduleName: 'Macy',
+  banner: '/* Macy.js - v2.0.0 */',
   plugins: [
-    babel(),
-    uglify()
+    babel()
   ],
   dest: 'dist/macy.js'
 };
+
+if (process.env.build !== 'dev') {
+  buildObj.plugins.push(uglify());
+}
+
+export default buildObj;
